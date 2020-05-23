f4c07ec36bfbe8e2e7831d6ae45c9279a4ec18f8
Giampiero Specogna <giampi_12@hotmail.com>
2020-05-23 11:11:13 -0400

    Commit for CI

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+        '197671975229-m8k69a0d2rv7dh60u19hgfoo3109de44.apps.googleusercontent.com',
+    googleClientSecret: 'gUSTR3STdm-LVuM8znBiAB-B',
+    mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+    cookieKey: '123123123',
+    redisUrl:'redis://127.0.0.1:6379'
+};
--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+    '197671975229-m8k69a0d2rv7dh60u19hgfoo3109de44.apps.googleusercontent.com',
+  googleClientSecret: 'gUSTR3STdm-LVuM8znBiAB-B',
+  mongoURI: "mongodb://giamp:azulado12@ds315359.mlab.com:15359/advancednodecourse",
   cookieKey: '123123123',
+  redisUrl:'redis://127.0.0.1:6379'
 };
